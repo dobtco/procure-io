ab2656e16f947500f3df0f01cbdee09acd080032
adamjacobbecker <ad@mbecker.cc>
2013-02-04 15:08:58 -0800

    add cancan

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.11'
 gem 'devise'
 gem 'foreigner'
 gem 'haml'
+gem 'cancan'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,20 @@
 class ProjectsController < ApplicationController
+  before_filter :project_exists?, except: :index
+
   def index
     @projects = Project.all
+  end
+
+  def show
+    if can? :update, @project
+      render "projects/show"
+    else
+      render "projects/show_public"
+    end
+  end
+
+  private
+  def project_exists?
+    @project = Project.find(params[:id])
   end
 end
