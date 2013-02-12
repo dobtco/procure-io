3e5d3e716b39924b68f8646588400b2ee4173dbb
adamjacobbecker <ad@mbecker.cc>
2013-02-11 20:58:40 -0800

    Collaborator spec

---
+++ spec/models/collaborator_spec.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: collaborators
+#
+#  id         :integer          not null, primary key
+#  project_id :integer
+#  officer_id :integer
+#  owner      :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'spec_helper'
+
+describe Collaborator do
+  subject { collaborators(:adamone) }
+
+  it { should respond_to(:owner) }
+
+  it { should respond_to(:project) }
+  it { should respond_to(:officer) }
+end
