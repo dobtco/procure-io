eb9de61d1149930f4f0ea86b260efb96e174796d
adamjacobbecker <ad@mbecker.cc>
2013-02-11 21:08:02 -0800

    question spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :integer          not null, primary key
+#  commentable_type :string(255)
+#  commentable_id   :integer
+#  officer_id       :integer
+#  vendor_id        :integer
+#  comment_type     :string(255)
+#  body             :text
+#  data             :text
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 require 'spec_helper'
 
 describe Comment do
