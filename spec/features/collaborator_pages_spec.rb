d9460ec1436c8e062cbdbdba3418fa00a50f46a9
adamjacobbecker <ad@mbecker.cc>
2013-02-12 18:00:46 -0800

    collaborator pages spec

---
+++ spec/features/collaborator_pages_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+
+describe "Collaborator" do
+
+  subject { page }
+
+  describe "index", js: true do
+    before do
+      login_as(officers(:adam), scope: :officer)
+      visit project_collaborators_path(projects(:one))
+    end
+
+    describe "basic rendering" do
+      # @todo something about owner
+      it { should have_selector('td', officers(:adam).email) }
+      it { should have_selector('td', officers(:clay).email) }
+    end
+
+    describe "removing collaborator" do
+      it "should remove collaborators from the list" do
+        page.should have_selector('#collaborators-tbody tr', count: 2)
+        find("#collaborators-tbody tr:eq(2) button").click
+        page.should have_selector('#collaborators-tbody tr', count: 1)
+        visit project_collaborators_path(projects(:one))
+        page.should have_selector('#collaborators-tbody tr', count: 1)
+      end
+    end
+
+    describe "adding duplicate collaborators" do
+      it "should not allow duplicates" do
+        page.should have_selector('#collaborators-tbody tr', count: 2)
+        fill_in "collaborator_officer_email", with: officers(:adam).email
+        click_button "Add Collaborator"
+        expect(page).to have_selector("#new_collaborator .btn:not(.disabled)")
+        page.should have_selector('#collaborators-tbody tr', count: 2)
+        visit project_collaborators_path(projects(:one))
+        page.should have_selector('#collaborators-tbody tr', count: 2)
+      end
+    end
+
+    describe "adding collaborators" do
+      before do
+        collaborators(:clayone).destroy
+        visit project_collaborators_path(projects(:one))
+      end
+
+      it "should allow you to add a new collaborator" do
+        page.should have_selector('td', officers(:adam).email)
+        page.should_not have_selector('td:contains("'+officers(:clay).email+'")')
+        fill_in "collaborator_officer_email", with: officers(:clay).email
+        click_button "Add Collaborator"
+        page.should have_selector('#collaborators-tbody tr', count: 2)
+        visit project_collaborators_path(projects(:one))
+        page.should have_selector('td', officers(:adam).email)
+        page.should have_selector('td', officers(:clay).email)
+      end
+    end
+  end
+
+end
