2ba49c7e36d4eebade6c7560d7407fb6058dfb67
adamjacobbecker <ad@mbecker.cc>
2013-03-06 12:58:03 -0800

    better error for officer auth when vendor logged in and vice-versa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,27 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include ApplicationHelper
 
+  alias_method :original_authenticate_vendor!, :authenticate_vendor!
+  alias_method :original_authenticate_officer!, :authenticate_officer!
+
+  def authenticate_vendor!
+    if officer_signed_in?
+      flash[:error] = "Sorry, that page is for vendors only."
+      redirect_to :root
+    else
+      original_authenticate_vendor!
+    end
+  end
+
+  def authenticate_officer!
+    if vendor_signed_in?
+      flash[:error] = "Sorry, that page is for officers only."
+      redirect_to :root
+    else
+      original_authenticate_officer!
+    end
+  end
+
   private
   def not_found
     raise ActionController::RoutingError.new('Not Found')
