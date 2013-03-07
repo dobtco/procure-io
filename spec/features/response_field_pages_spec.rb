3352a73f872a24c30c31a6802a042bf8addc3999
adamjacobbecker <ad@mbecker.cc>
2013-03-07 09:05:22 -0800

    fix tests

--- spec/features/response_field_pages_spec.rb
+++ spec/features/response_field_pages_spec.rb
@@ -14,15 +14,15 @@ describe "ResponseField" do
       before { visit project_response_fields_path(projects(:one)) }
 
       it "should render properly" do
-        page.should have_selector(".response-field:eq(1)", text: response_fields(:one).label)
-        page.should have_selector(".response-field:eq(1) input[type=text]")
-        page.should have_selector(".response-field:eq(2)", text: response_fields(:two).label)
-        page.should have_selector(".response-field:eq(2) textarea")
+        page.should have_selector(".response-field-wrapper:eq(1)", text: response_fields(:one).label)
+        page.should have_selector(".response-field-wrapper:eq(1) input[type=text]")
+        page.should have_selector(".response-field-wrapper:eq(2)", text: response_fields(:two).label)
+        page.should have_selector(".response-field-wrapper:eq(2) textarea")
       end
 
       it "should be able to remove fields" do
         page.should have_selector("label", text: response_fields(:one).label)
-        find(".response-field:eq(1) .remove-field-button").click
+        find(".response-field-wrapper:eq(1) .remove-field-button").click
         page.should_not have_selector("label", text: response_fields(:one).label)
         visit project_response_fields_path(projects(:one))
         page.should_not have_selector("label", text: response_fields(:one).label)
@@ -30,61 +30,62 @@ describe "ResponseField" do
 
       describe "adding fields" do
         it "should be able to add a text field" do
-          page.should have_selector(".response-field input[type=text]", count: 1)
+          page.should have_selector(".response-field-wrapper input[type=text]", count: 1)
           find("[data-backbone-add-field=text]").click
-          page.should have_selector(".response-field input[type=text]", count: 2)
+          page.should have_selector(".response-field-wrapper input[type=text]", count: 2)
           visit project_response_fields_path(projects(:one))
-          page.should have_selector(".response-field input[type=text]", count: 2)
+          page.should have_selector(".response-field-wrapper input[type=text]", count: 2)
         end
 
         it "should be able to add a paragraph field" do
-          page.should have_selector(".response-field textarea", count: 1)
+          page.should have_selector(".response-field-wrapper textarea", count: 1)
           find("[data-backbone-add-field=paragraph]").click
-          page.should have_selector(".response-field textarea", count: 2)
+          page.should have_selector(".response-field-wrapper textarea", count: 2)
           visit project_response_fields_path(projects(:one))
-          page.should have_selector(".response-field textarea", count: 2)
+          page.should have_selector(".response-field-wrapper textarea", count: 2)
         end
       end
 
       describe "editing fields" do
         it "should show the correct edit panel when a field is clicked" do
           page.should_not have_selector("#edit-response-field-wrapper", visible: true)
-          find(".response-field:eq(1) .subtemplate-wrapper").click
+          find(".response-field-wrapper:eq(1) .subtemplate-wrapper").click
           page.should have_selector("#edit-response-field-wrapper", visible: true)
           page.should have_text("Editing \"#{response_fields(:one).label}\"")
-          find(".response-field:eq(2) .subtemplate-wrapper").click
+          find(".response-field-wrapper:eq(2) .subtemplate-wrapper").click
           page.should have_text("Editing \"#{response_fields(:two).label}\"")
         end
 
         describe "bindings" do
-          before { find(".response-field:eq(1) .subtemplate-wrapper").click }
+          before { find(".response-field-wrapper:eq(1) .subtemplate-wrapper").click }
 
           it "should bind the label field" do
             find("[data-rv-value=\"model.label\"]").set("NewLabel")
-            page.should have_selector(".response-field:eq(1)", text: "NewLabel")
+            page.should have_selector(".response-field-wrapper:eq(1)", text: "NewLabel")
           end
 
           it "should bind the description field" do
             find("[data-rv-value=\"model.field_options.description\"]").set("TheDescription")
-            page.should have_selector(".response-field:eq(1)", text: "TheDescription")
+            page.should have_selector(".response-field-wrapper:eq(1)", text: "TheDescription")
           end
 
           it "should bind the required field" do
-            page.should have_selector(".response-field:eq(1) .required-asterisk", visible: true)
+            page.should have_selector(".response-field-wrapper:eq(1) .required-asterisk")
             find("[data-rv-checked=\"model.field_options.required\"]").set(false)
-            page.should have_selector(".response-field:eq(1) .required-asterisk", visible: false)
+            page.should_not have_selector(".response-field-wrapper:eq(1) .required-asterisk")
           end
 
           it "should save properly" do
             find("[data-rv-value=\"model.label\"]").set("NewLabel2")
             find("[data-rv-value=\"model.field_options.description\"]").set("TheDescription")
             find("[data-rv-checked=\"model.field_options.required\"]").set(false)
-            click_button "Save Form"
             expect(page).to have_selector("[data-backbone-save-form]:not(.disabled)")
+            click_button "Save Form"
+            expect(page).to have_selector(".disabled[data-backbone-save-form]")
             visit project_response_fields_path(projects(:one))
-            page.should have_selector(".response-field:eq(1)", text: "NewLabel2")
-            page.should have_selector(".response-field:eq(1)", text: "TheDescription")
-            page.should have_selector(".response-field:eq(1) .required-asterisk", visible: false)
+            page.should have_selector(".response-field-wrapper:eq(1)", text: "NewLabel2")
+            page.should have_selector(".response-field-wrapper:eq(1)", text: "TheDescription")
+            page.should_not have_selector(".response-field-wrapper:eq(1) .required-asterisk")
           end
         end
       end
