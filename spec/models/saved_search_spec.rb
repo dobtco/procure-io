241853ab02526bc8ff08f3f81175d8fc7b7b9e05
adamjacobbecker <ad@mbecker.cc>
2013-03-07 21:59:29 -0800

    fix specs

--- spec/models/saved_search_spec.rb
+++ spec/models/saved_search_spec.rb
@@ -25,13 +25,13 @@ describe SavedSearch do
 
   describe "execute" do
     it "should execute properly" do
-      saved_searches(:one).execute.results.first.should == projects(:one)
+      saved_searches(:one).execute[:results].first.should == projects(:one)
     end
 
     describe "execute since last search" do
       before { saved_searches(:one).update_attributes(last_emailed_at: Time.now) }
       it "should execute properly" do
-        saved_searches(:one).execute_since_last_search.results.first.should == nil
+        saved_searches(:one).execute_since_last_search[:results].first.should == nil
       end
     end
   end
