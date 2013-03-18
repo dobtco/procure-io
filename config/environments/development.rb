03f400e2a39604331dd90c5fe7827218e7f7f177
adamjacobbecker <ad@mbecker.cc>
2013-03-18 14:09:03 -0700

    config.assets.debug = false solves EXTREMELY slow load times in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ ProcureIo::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 
   config.action_mailer.default_url_options = { host: 'procure-io.dev' }
 
