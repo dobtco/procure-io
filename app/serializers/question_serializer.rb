c3a9332ca01adb01973f1eb155e866b0454de78e
adamjacobbecker <ad@mbecker.cc>
2013-03-22 12:11:40 -0700

    UI for questions_admin page

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,3 +1,7 @@
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :updated_at, :body, :answer_body
+  attributes :id, :created_at, :updated_at, :body, :answer_body, :created_at_readable
+
+  def created_at_readable
+    object.created_at.to_formatted_s(:readable) if object.created_at
+  end
 end
