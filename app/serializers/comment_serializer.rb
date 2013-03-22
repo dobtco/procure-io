c5612a30a70b313763aa27ee548874aeb807b99e
adamjacobbecker <ad@mbecker.cc>
2013-03-22 15:21:15 -0700

    use readable created at

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,7 +1,12 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :body, :comment_type, :commentable_id, :commentable_type, :data, :officer_id, :vendor_id, :created_at
+  attributes :id, :body, :comment_type, :commentable_id, :commentable_type, :data, :officer_id, :vendor_id,
+             :created_at, :created_at_readable
 
   has_one :commentable
   has_one :officer
   has_one :vendor
+
+  def created_at_readable
+    object.created_at.to_formatted_s(:readable) if object.created_at
+  end
 end
