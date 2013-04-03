c388e2ae7be6c93debfc3ee1cc76527f7d65b8e7
adamjacobbecker <ad@mbecker.cc>
2013-04-03 10:39:31 -0700

    proof of concept for better-looking vendor signup page

--- app/controllers/vendors/registrations_controller.rb
+++ app/controllers/vendors/registrations_controller.rb
@@ -2,4 +2,10 @@ class Vendors::RegistrationsController < Devise::RegistrationsController
   def edit
     not_found
   end
+
+  def resource_params
+    params.require(:vendor).permit(:name, :email, :password, :current_password)
+  end
+
+  private :resource_params
 end
