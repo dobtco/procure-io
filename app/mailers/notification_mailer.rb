00a5c80b2afce02ca83c921605f26de794a8295d
adamjacobbecker <ad@mbecker.cc>
2013-04-07 14:29:09 -0700

    more pagination

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,14 @@
 class NotificationMailer < ActionMailer::Base
+  include EmailBuilder
+
   default from: "from@example.com"
 
   def notification_email(user, event)
-    @event = event
-    @user = user
-    mail(to: user.email, subject: event.text)
+    build_email user.email,
+                'notification',
+                event_text: event.text,
+                event_additional_text: event.additional_text,
+                name: user.display_name,
+                event_url: URI.join(root_url, event.path)
   end
 end
