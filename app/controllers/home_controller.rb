fa1e0293fe747e2edf60377e0039ff9d1d81b20f
adamjacobbecker <ad@mbecker.cc>
2013-04-07 13:31:44 -0700

    only show projects open for bids on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     @featured_projects = Project.posted.featured.order("RANDOM()").limit(3)
 
     if @featured_projects.length < 3
-      @featured_projects = @featured_projects + Project.posted.order("RANDOM()").limit(3 - @featured_projects.length)
+      @featured_projects = @featured_projects + Project.open_for_bids.posted.order("RANDOM()").limit(3 - @featured_projects.length)
     end
   end
 end
