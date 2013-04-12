8010742c93dc01f6ff763d339f4840b0cef37cc2
adamjacobbecker <ad@mbecker.cc>
2013-04-12 07:20:52 -0700

    add fallback secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ProcureIo::Application.config.secret_token = ENV['SECRET_TOKEN']
+ProcureIo::Application.config.secret_token = ENV['SECRET_TOKEN'] || "98984c304f5815d1c3862ebd0333590b2f5bd90115edbfea8ad0cda843156d7122f3a43e7cdec119300fbe5228eba4f8acce32138185d305c52847b73d527533"
