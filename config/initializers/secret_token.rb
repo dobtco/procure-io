8cb30b2f44155c7a4585da651dd819f568f5f97f
adamjacobbecker <ad@mbecker.cc>
2013-04-12 07:20:08 -0700

    secret_token -> env vars

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ProcureIo::Application.config.secret_token = 'f9ea30cc6e787eb0fc1eb775a958051e70037305339608f075591057e83adf1a9009baa09d9480b3b992d1d9e82068befecd8d28db9d663e3b9377bd595136d4'
+ProcureIo::Application.config.secret_token = ENV['SECRET_TOKEN']
