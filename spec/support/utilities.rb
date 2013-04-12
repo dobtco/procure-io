f7df27e777aa8d3e8de30ce8db095a0c052ca5e8
adamjacobbecker <ad@mbecker.cc>
2013-04-11 17:09:50 -0700

    remove more traces of warden

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,18 +1,1 @@
 include ApplicationHelper
-include Warden::Test::Helpers
-Warden.test_mode!
-
-RSpec::Matchers.define :have_error_message do |message|
-  match do |page|
-    page.should have_selector('div.alert.alert-error', text: message)
-  end
-end
-
-RSpec::Matchers.define :be_accessible do |attribute|
-  match do |response|
-    response.class.accessible_attributes.include?(attribute)
-  end
-  description { "be accessible :#{attribute}" }
-  failure_message_for_should { ":#{attribute} should be accessible" }
-  failure_message_for_should_not { ":#{attribute} should not be accessible" }
-end
