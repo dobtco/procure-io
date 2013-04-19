c322c5451b29992ea1d64473c0acf881b9d86a3d
adamjacobbecker <ad@mbecker.cc>
2013-04-18 20:54:36 -0700

    collaborator cleanup

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,7 +1,7 @@
 class CollaboratorsController < ApplicationController
-  before_filter :project_exists?
-  before_filter :authenticate_officer!
-  before_filter :authorize_officer!
+  load_resource :project
+  before_filter { |c| c.authorize! :admin, @project }
+  load_resource :collaborator, through: :project, only: [:destroy]
 
   def index
     current_user.read_notifications(@project, :collaborator_added, :you_were_added)
@@ -12,38 +12,23 @@ class CollaboratorsController < ApplicationController
     emails = create_collaborator_params[:email].split(',').map{ |e| e.strip }
 
     emails.each do |email|
-      officer = Officer.joins(:user).where(users: { email: email }).first
-      officer = Officer.invite!(email, @project, create_collaborator_params[:role_id]) if !officer
+      officer = Officer.joins(:user).where(users: { email: email }).first ||
+                Officer.invite!(email, @project, create_collaborator_params[:role_id])
 
-      officer.collaborators.where(project_id: @project.id).first_or_create(added_by_officer_id: current_officer.id) if officer.valid?
+      if officer.valid?
+        officer.collaborators.where(project_id: @project.id).first_or_create(added_by_officer_id: current_officer.id)
+      end
     end
-
     @collaborators = @project.collaborators
-
-    respond_to do |format|
-      format.js
-    end
   end
 
   def destroy
-    authorize! :destroy, @project # only the owner of the project can remove collaborators
-    @project.collaborators.find(params[:id]).destroy
-
-    respond_to do |format|
-      format.json { render json: {} }
-    end
+    authorize! :destroy, @collaborator
+    @collaborator.destroy
+    render json: {}
   end
 
   private
-  def project_exists?
-    @project = Project.find(params[:project_id])
-    authorize! :collaborate_on, @project
-  end
-
-  def authorize_officer!
-    authorize! :admin, @project
-  end
-
   def create_collaborator_params
     filtered_params = pick(params, :email, :role_id)
 
