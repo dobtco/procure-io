f6a3877b5fc56accc2af2b050edcc3b13b3dec84
adamjacobbecker <ad@mbecker.cc>
2013-04-19 15:31:11 -0700

    tweaks

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,7 +1,7 @@
 module I18n
-  def self.just_raise_that_exception(*args)
-    raise args.first
-  end
+  def self.just_raise_that_exception(*args)
+    raise args.first
+  end
 end
- 
+
 I18n.exception_handler = :just_raise_that_exception
