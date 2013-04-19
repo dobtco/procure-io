99b37c5780292d3351dc940d1299b456735828b0
adamjacobbecker <ad@mbecker.cc>
2013-04-19 07:57:34 -0700

    cleanup project revisions

--- app/controllers/project_revisions_controller.rb
+++ app/controllers/project_revisions_controller.rb
@@ -1,8 +1,10 @@
 class ProjectRevisionsController < ApplicationController
-  before_filter :project_exists?
-  before_filter :project_revision_exists?
-  before_filter :authenticate_officer!
-  before_filter :authorize_officer!
+  # Load
+  load_resource :project
+  load_resource :project_revision, through: :project
+
+  # Authorize
+  before_filter { |c| c.authorize! :collaborate_on, @project }
 
   def show
   end
@@ -11,18 +13,5 @@ class ProjectRevisionsController < ApplicationController
     @project.updating_officer_id = current_officer.id
     @project.update_attributes(body: @project_revision.body)
     redirect_to edit_project_path(@project)
-  end
-
-  private
-  def project_exists?
-    @project = Project.find(params[:project_id])
-  end
-
-  def project_revision_exists?
-    @project_revision = @project.project_revisions.find(params[:id])
-  end
-
-  def authorize_officer!
-    authorize! :collaborate_on, @project
   end
 end
