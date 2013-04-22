e123077e072404b1f935386202595c84840a7643
adamjacobbecker <ad@mbecker.cc>
2013-04-21 21:38:53 -0700

    db:seed:development task

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,17 +15,3 @@ Role.create(name: "Supervisor", role_type: Role.role_types[:user], undeletable: 
 Role.create(name: "Admin", role_type: Role.role_types[:admin], undeletable: true)
 
 Role.create(name: "God", role_type: Role.role_types[:god], undeletable: true)
-
-unless Rails.env.production?
-  Delayed::Worker.delay_jobs = false
-  ActionMailer::Base.perform_deliveries = false
-
-  3.times { FactoryGirl.create(:officer) }
-  30.times { FactoryGirl.create(:vendor) }
-  10.times { FactoryGirl.create(:tag) }
-  3.times { FactoryGirl.create(:project_with_bids) }
-  2.times { FactoryGirl.create(:amendment) }
-  5.times { FactoryGirl.create(:comment) }
-  20.times { FactoryGirl.create(:question) }
-  20.times { FactoryGirl.create(:project) }
-end
---
+++ lib/tasks/db_seed_development.rake
@@ -0,0 +1,21 @@
+namespace :db do
+  namespace :seed do
+
+    desc "seed bogus data"
+    task development: :environment do
+
+      Delayed::Worker.delay_jobs = false
+      ActionMailer::Base.perform_deliveries = false
+
+      3.times { FactoryGirl.create(:officer) }
+      30.times { FactoryGirl.create(:vendor) }
+      10.times { FactoryGirl.create(:tag) }
+      3.times { FactoryGirl.create(:project_with_bids) }
+      2.times { FactoryGirl.create(:amendment) }
+      5.times { FactoryGirl.create(:comment) }
+      20.times { FactoryGirl.create(:question) }
+      20.times { FactoryGirl.create(:project) }
+    end
+
+  end
+end
