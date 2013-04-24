dec3692098404fb34509fd4deb4f770037ace86d
adamjacobbecker <ad@mbecker.cc>
2013-04-23 19:43:14 -0700

    upgrade to ruby 2.0.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,9 @@ require 'simplecov'
 SimpleCov.start
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV["RAILS_ENV"] = 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara/rspec'
 require 'database_cleaner'
 
