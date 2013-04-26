3dfc2ca9feb538a958107fede42cb07bfd107b47
adamjacobbecker <ad@mbecker.cc>
2013-04-26 13:16:37 -0700

    more

--- spec/behaviors/awardable_and_dismissable_by_officer_spec.rb
+++ spec/behaviors/awardable_and_dismissable_by_officer_spec.rb
@@ -5,6 +5,9 @@ class Model < FakeModel
   include Behaviors::AwardableAndDismissableByOfficer
 end
 
+class I18n
+end
+
 describe Behaviors::AwardableAndDismissableByOfficer do
 
   before { @model = Model.new }
@@ -100,7 +103,26 @@ describe Behaviors::AwardableAndDismissableByOfficer do
   end
 
   describe '#text_status' do
-    it 'should return the proper status'
+    it 'should return the proper status when dismissed' do
+      I18n.should_receive(:t).with("g.dismissed")
+      @model.awarded_at = Time.now
+      @model.dismissed_at = Time.now
+      @model.text_status
+    end
+
+    it 'should return the proper status when awarded' do
+      I18n.should_receive(:t).with("g.awarded")
+      @model.dismissed_at = nil
+      @model.awarded_at = Time.now
+      @model.text_status
+    end
+
+    it 'should return the proper status when open' do
+      I18n.should_receive(:t).with("g.open")
+      @model.dismissed_at = nil
+      @model.awarded_at = nil
+      @model.text_status
+    end
   end
 
 end
