b64d4267620c25d80277e4b13efc6c289f06129e
adamjacobbecker <ad@mbecker.cc>
2013-05-01 09:54:40 -0700

    major cleanup of bid review spec

--- spec/features/bid_review_spec.rb
+++ spec/features/bid_review_spec.rb
@@ -9,35 +9,34 @@ describe "Bid Review", js: true do
   describe "bids/index" do
     before do
       15.times { |i| FactoryGirl.create(:bid, project: projects(:one), submitted_at: Time.now + i.seconds) }
-      visit project_bids_path(projects(:one))
+      visit project_bids_path(projects(:one), sort: "created_at", direction: "asc")
     end
 
     it "should show 10 bids" do
       page.should have_selector('#bids-tbody tr', count: 10)
     end
 
-    # @todo sorting
     it "should sort"
 
-    # @todo we *should* test for > 10 pages
     describe "pagination" do
       it "should render the pagination" do
-        page.should have_selector('li.active a', '1')
-        page.should have_selector('li a', '2')
+        pagination_should_be_on_page(1)
       end
+
+      it "should render longer paginations"
 
       it "should paginate properly" do
-        find(".pagination a:contains(2)").click
-        expect(page).to have_selector('.loading-indicator', visible: true)
-        page.should have_selector('li a', '1')
-        page.should have_selector('li.active a', '2')
-        page.should_not have_selector('[href="'+project_bid_path(projects(:one), projects(:one).bids.last)+'"]')
-        page.should have_selector('[href="'+project_bid_path(projects(:one), projects(:one).bids.first)+'"]')
+        find(".pagination").click_link("2")
+        wait_for_load
+        pagination_should_be_on_page(2)
+        page.should_not have_bid_link(projects(:one).bids.first)
+        page.should have_bid_link(projects(:one).bids.last)
       end
 
-      # @todo this would be a better test with bids in each category.
       it "should remove pagination when clicking on another filter" do
-        page.should have_selector('li.active a', '2')
+        find(".pagination").click_link("2")
+        wait_for_load
+        pagination_should_be_on_page(2)
         find('a:contains("Dismissed Bids")')[:href].should_not match("page=")
       end
     end
@@ -46,9 +45,9 @@ describe "Bid Review", js: true do
       describe "starring" do
         it "should only show starred bids" do
           click_link "Starred Bids"
-          page.should have_selector('[href="'+project_bid_path(projects(:one), bids(:one))+'"]')
-          page.should_not have_selector('[href="'+project_bid_path(projects(:one), projects(:one).bids.where(total_stars: 0).first)+'"]')
-          page.should_not have_selector('[href="'+project_bid_path(projects(:one), projects(:one).bids.where(total_stars: 0).last)+'"]')
+          page.should have_bid_link(bids(:one))
+          page.should_not have_bid_link(projects(:one).bids.where(total_stars: 0).first)
+          page.should_not have_bid_link(projects(:one).bids.where(total_stars: 0).last)
         end
       end
 
@@ -57,8 +56,8 @@ describe "Bid Review", js: true do
 
         it "should only show dismissed bids" do
           click_link "Dismissed Bids"
-          page.should have_selector('[href="'+project_bid_path(projects(:one), bids(:one))+'"]')
-          page.should_not have_selector('[href="'+project_bid_path(projects(:one), projects(:one).bids.first)+'"]')
+          page.should have_bid_link(projects(:one).bids.dismissed.first)
+          page.should_not have_bid_link(projects(:one).bids.where_open.first)
         end
       end
     end
@@ -68,8 +67,7 @@ describe "Bid Review", js: true do
         it "should correctly mark multiple bids as dismissed" do
           all("#bids-tbody input[type=checkbox]").each { |e| e.set(true) }
           click_button "Dismiss"
-          page.should have_selector('.loading-indicator', visible: true)
-          page.should have_selector('#bid-review-page:not(.loading)')
+          wait_for_load
           page.should have_selector('.pagination ul li', count: 3)
         end
       end
@@ -85,86 +83,91 @@ describe "Bid Review", js: true do
   end
 
   describe "bids/show" do
+    before do
+      visit project_bid_path(projects(:one), bids(:one))
+    end
+
     describe "when vendor has not submitted bid" do
       it "should render 404", js: false do
         bids(:one).update_attributes(submitted_at: nil)
-        expect { visit project_bid_path(projects(:one), bids(:one)) }.to raise_error(ActionController::RoutingError)
+        expect { visit project_bid_path(projects(:one), bids(:one)) }.to render_404
       end
     end
 
     describe "basic rendering" do
-      before { visit project_bid_path(projects(:one), bids(:one)) }
-      it { should have_text(bids(:one).vendor.name) }
+      it "should show the vendor's name" do
+        page.should have_text(bids(:one).vendor.name)
+      end
+
       it "should show all responses" do
         bids(:one).responses.each do |response|
-          page.should have_selector('dd', response.value)
+          page.should have_selector('dd', text: response.value)
         end
       end
     end
 
     describe "dismissal" do
-      before { visit project_bid_path(projects(:one), bids(:one)) }
-      it { should have_selector('.badge:contains("Open")') }
-      it "should dismiss bids properly" do
-        click_button "Dismiss"
-        page.should have_selector('.badge:contains("Dismissed")')
-        visit project_bid_path(projects(:one), bids(:one))
-        page.should have_selector('.badge:contains("Dismissed")')
+      it "should be open" do
+        page.should have_selector('.badge', text: 'Open')
       end
 
-      describe "undismissal" do
-        before do
-          bids(:one).dismiss_by_officer!(officers(:adam))
-          visit project_bid_path(projects(:one), bids(:one))
+      it "should dismiss bids properly" do
+        click_button "Dismiss"
+        before_and_after_refresh do
+          page.should have_selector('.badge', text: 'Dismissed')
         end
-        it "should undismiss bids properly"
       end
     end
 
-    describe "starring" do
-      before { visit project_bid_path(projects(:one), bids(:one)) }
-      it { should have_selector('[data-backbone-star]') }
+    describe "undismissal" do
+      before { bids(:one).dismiss_by_officer!(officers(:adam)) }
+      it "should undismiss bids properly"
+    end
 
-      it "should recalculate star count asynchronously" do
+    describe "starring" do
+      it "should be starred" do
+        ensure_bid_is_starred
       end
+
+      it "should recalculate star count asynchronously"
 
       it "should save star count when refreshing" do
         find('[data-backbone-star]').click
-        visit project_bid_path(projects(:one), bids(:one))
-        page.should have_selector('.icon-star-empty')
+        before_and_after_refresh do
+          ensure_bid_is_unstarred
+        end
       end
     end
 
     describe "unread" do
-      before { visit project_bid_path(projects(:one), bids(:one)) }
       it "should mark as read when reloading the page"
     end
 
     describe "comments" do
-      before { visit project_bid_path(projects(:one), bids(:one)) }
-      it { should have_selector('.comment', comments(:one).body) }
+      it "should render the comment body" do
+        page.should have_selector('.comment', comments(:one).body)
+      end
 
       it "should create a new comment" do
         find("#new-comment-form textarea").set("Hey dudes.")
         click_button "Post Comment"
-        page.should have_selector('.comment', text: 'Hey dudes.')
-        visit project_bid_path(projects(:one), bids(:one))
-        page.should have_selector('.comment', text: 'Hey dudes.')
+        before_and_after_refresh do
+          page.should have_selector('.comment', text: 'Hey dudes.')
+        end
       end
 
       describe "deleting comments" do
         it "should let you delete your own comments" do
-          page.should have_selector('.comment', text: comments(:one).body)
           find(".comment a.delete").click
-          page.should_not have_selector('.comment', text: comments(:one).body)
-          visit project_bid_path(projects(:one), bids(:one))
-          page.should_not have_selector('.comment', text: comments(:one).body)
+          before_and_after_refresh do
+            page.should_not have_selector('.comment', text: comments(:one).body)
+          end
         end
 
         it "should not let you delete other officers' comments" do
           sign_out
           sign_in(officers(:clay).user)
-          visit project_bid_path(projects(:one), bids(:one))
+          refresh
           page.should have_selector('.comment', text: comments(:one).body)
           page.should_not have_selector('.comment a.delete')
         end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -17,3 +17,47 @@ end
 def sign_out
   UserSession.stub!(:find).and_return(nil)
 end
+
+def ensure_loading
+  expect(page).to have_selector('.loading-indicator', visible: true)
+end
+
+def ensure_done_loading
+  expect(page).to have_selector('.loading-indicator', visible: false)
+end
+
+def wait_for_load
+  ensure_loading
+  ensure_done_loading
+end
+
+def refresh
+  visit current_path
+end
+
+def before_and_after_refresh(&block)
+  instance_eval(&block)
+  refresh
+  instance_eval(&block)
+end
+
+def have_bid_link(bid)
+  have_selector('[href="'+project_bid_path(bid.project, bid)+'"]')
+end
+
+def pagination_should_be_on_page(page_number)
+  expect(page).to have_selector('.pagination')
+  page.should have_selector('li.active a', text: page_number.to_s)
+end
+
+def ensure_bid_is_starred
+  page.should have_selector('[data-backbone-star].icon-star')
+end
+
+def ensure_bid_is_unstarred
+  page.should have_selector('[data-backbone-star].icon-star-empty')
+end
+
+def render_404
+  raise_error(ActionController::RoutingError)
+end
