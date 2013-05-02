09e6d6b0e3d446589a697f41c12075974a17b808
adamjacobbecker <ad@mbecker.cc>
2013-05-02 16:18:33 -0700

    revamped home pages spec

--- spec/features/home_pages_spec.rb
+++ spec/features/home_pages_spec.rb
@@ -2,28 +2,47 @@ require 'spec_helper'
 
 describe "Home" do
 
-  subject { page }
-
-  describe "root logged out" do
-    before { visit root_path }
-
-    it { should have_link('Sign in') }
-  end
-
-  describe "root logged in" do
+  describe 'the root page' do
     before do
-      sign_in(users(:adam_user))
+      @projects = []
+      3.times { @projects.push Project.create(posted_at: Time.now, title: 'Foo', abstract: 'Bar', featured: true) }
       visit root_path
     end
 
-    it { should have_selector("ul.nav.pull-right", text: officers(:adam).name) }
-
-    describe "it should show email address if user doesnt have a name set" do
-      before do
-        officers(:adam).update_attributes(name: nil)
+    it 'should show featured projects first' do
+      3.times do # to account for random selection of featured projects
         visit root_path
+        @projects.each do |project|
+          page.should have_selector("a[href=\"#{project_path(project)}\"]")
+        end
       end
-      it { should have_selector("ul.nav.pull-right", text: officers(:adam).user.email) }
+    end
+
+    it 'should show non-featured projects if there are not enough featured projects' do
+      @projects.each { |p| p.update_attributes(featured: false) }
+      refresh
+      page.should have_selector(".featured-projects .project", count: 3)
+    end
+
+  end
+
+  describe 'the navbar' do
+    before do
+      visit root_path
+    end
+
+    context 'when logged out' do
+      it 'should have a signin link' do
+        page.should have_selector("a", text: I18n.t('g.sign_in'))
+      end
+    end
+
+    context 'when logged in' do
+      it 'should show the users display_name' do
+        sign_in(officers(:adam).user)
+        refresh
+        page.should have_text(officers(:adam).display_name)
+      end
     end
   end
 
