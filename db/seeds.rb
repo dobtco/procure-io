0a9de831ed7c27cd70d40a39eb50d51f05724430
adamjacobbecker <ad@mbecker.cc>
2013-05-15 15:47:03 -0700

    create some form templates with the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,33 @@ Role.create(name: "Supervisor", role_type: Role.role_types[:user],
 Role.create(name: "Admin", role_type: Role.role_types[:admin], undeletable: true)
 
 Role.create(name: "God", role_type: Role.role_types[:god], undeletable: true)
+
+form_template_file_upload = FormTemplate.create(name: "Upload File (not recommended)",
+                                                form_options: { "form_description" => "Attach your proposal below."})
+
+form_template_file_upload.response_fields.create(label: "Upload your proposal",
+                                                 field_type: "file",
+                                                 field_options: { "required" => true },
+                                                 sort_order: 0)
+
+form_template_rfpez = FormTemplate.create(name: "RFP-EZ Form")
+
+form_template_rfpez.response_fields.create(label: "Your Approach",
+                                           field_type: "paragraph",
+                                           field_options: {"size" => "large", "required" => true, "description" => "How would you complete this project?"},
+                                           sort_order: 0)
+
+form_template_rfpez.response_fields.create(label: "Previous Work",
+                                           field_type: "paragraph",
+                                           field_options: {"size" => "large", "required" => true, "description" => "What qualifies you to work on this project?"},
+                                           sort_order: 1)
+
+form_template_rfpez.response_fields.create(label: "Employee Details",
+                                           field_type: "paragraph",
+                                           field_options: {"size" => "medium", "required" => true, "description" => "Who would work on this project?"},
+                                           sort_order: 2)
+
+form_template_rfpez.response_fields.create(label: "Total Cost",
+                                           field_type: "price",
+                                           sort_order: 3,
+                                           field_options: {"required" => true})
